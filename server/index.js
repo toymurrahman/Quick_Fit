67fc0c114d640f5cbcd0963d60e58b348b296275
toymurrahman <rahmantoymur2@gmail.com>
2025-05-17 23:15:38 +0600

    server api intregated

--- server/index.js
+++ server/index.js
@@ -43,6 +43,29 @@ const is_live = false //true for live, false for sandbox
 //************   All APi's Starts   ************************//
 
 
+
+
+
+const { getMonthlyData, getMonthlySigleData, addMonthlyPicks, getMonthlyAuthorWiseData } = require('./APi/MonthlyPicks/monthlyController');
+const { getNewStories, getSingleStory, getAuthorWiseStory, addStory } = require('./APi/NewsStories/newsStoriesController');
+const { getSpotlightData, getSpotlightSingleData } = require('./APi/Spotlight/spotlightController');
+const { getCategoryData, getSingleCategoryData } = require('./APi/Category/categoryController');
+const { getArticleData, getArticleSingleData } = require('./APi/article/articleController');
+const { getEshopData, getEshopSingleData, getEshopAllData } = require('./APi/EshopProducts/eshopController');
+const { getCartData, postCartData, getCartAllData, deleteCartData } = require('./APi/cart/cartController');
+const { getBookData, getSingleBookData } = require('./APi/books/booksController');
+const { getTipsData } = require('./APi/tips/tipsController');
+
+const { addFavourites, getFavourites, deleteFavourites } = require('./APi/Favourite/favouriteController');
+const { updateLikes } = require('./APi/userInteraction/userInteractionController');
+const { addComment, getComment } = require('./APi/Comment/commentController');
+
+const { getTeamsData } = require('./APi/teams/teamsController');
+const { getExpertsData } = require('./APi/experts/expertsController');
+const { getServicesData } = require('./APi/services/services');
+const { postTrainerData } = require('./APi/Trainer/teainerHire');
+
+
 // ---------Payment Method APi's starts ------------
 
 //article api's starts--------
@@ -255,6 +278,22 @@ app.get("/api/v1/services", async (req, res) => {
 
  
    
+app.post("/api/v1/users", async(req,res)=>{
+    const data = req.body;
+    console.log(data);
+    const result = await addUser(data);
+    res.send(result);
+})
+
+app.get("/api/v1/users", async(req,res)=>{
+    const result = await getAllUser();
+    res.send(result);
+})
+
+app.get("/api/v1/users/:email", async(req,res)=>{
+    const result = await getSingleUser(req.params.email);
+    res.send(result);
+})
 
 
 //Forum Ends here
